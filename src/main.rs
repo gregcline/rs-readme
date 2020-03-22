1fbec27e0039d7781e1d5debed83ba05fd1caf35
Greg Cline <gregrcline@gmail.com>
2020-03-22 17:16:30 -0400

    The default endpoint renders README
    The default endpoint is hardcoded to use ./README.md so you better have
    one. It also has no useful error handling, everything is just a 500
    right now.
    
     Point of note, because `tide` uses `async_std` and `reqwests` uses
    `tokio` for their respective runtimes they don't play nicely together.
    From what I read on reddit (-_-) I would have to start a background
    thread using with the tokio runtime and pass all uses of reqwests to
    that thread but it seemed overly complicated for what I want to do.
    Therefore I switched to `surf` since it runs on the `async_std` runtime.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate async_std;
+
+use surf;
+use tide::{Request, Response, Result, ResultExt, Server,};
+
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MarkdownRequest {
+    text: String,
+    mode: String,
+    context: String,
+}
+
+async fn find_readme(_req: Request<()>) -> Result {
+    let path = Path::new("README.md");
+
+    let mut file = File::open(path).unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).server_err()?;
+
+    let client = surf::Client::new();
+
+    let resp = client.post("https://api.github.com/markdown")
+        .body_json(&MarkdownRequest {
+            text: contents,
+            mode: "markdown".to_string(),
+            context: "".to_string(),
+        })
+        .server_err()?
+        .recv_string()
+        .await
+        .map_err(|_| Response::new(500))?;
+
+    Ok(Response::new(200).body_string(resp))
+}
+
+#[async_std::main]
+async fn main() -> std::result::Result<(), std::io::Error> {
+    let addr = format!("0.0.0.0:{}", env::var("PORT").unwrap_or_else(|_| "4000".to_string()));
+
+    println!("Listening on http://{}", addr).await;
+
+    let mut app = Server::new();
+    app.at("").get(find_readme);
+    app.listen(addr).await
 }
